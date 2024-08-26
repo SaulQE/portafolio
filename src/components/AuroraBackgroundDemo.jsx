f404d9fdac5e0c54bc71af824774c160a989ab7d
SaulQE <saul.esquivel.028@gmail.com>
2024-08-26 17:00:35 -0500

    add comentario

--- src/components/AuroraBackgroundDemo.jsx
+++ src/components/AuroraBackgroundDemo.jsx
@@ -18,7 +18,7 @@ export function AuroraBackgroundDemo() {
 					Background lights are cool you know.
 				</div>
 				<div className='font-extralight text-base md:text-4xl dark:text-neutral-200 py-4'>
-					And this, is chemical burn.
+					Portafolio en desarrollo...
 				</div>
 				<button className='bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2'>
 					Debug now
