704c68789ac4015d60e0a388f0b7f2bde2be02dc
SaulQE <saul.esquivel.028@gmail.com>
2024-08-26 17:00:13 -0500

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,32 @@ import { ThreeDCardDemo } from './components/ThreeDCardDemo';
 import { BentoGridDemo } from './components/BentoGridDemo';
 import { HeroParallaxDemo } from './components/HeroParallaxDemo';
 import { AuroraBackgroundDemo } from './components/AuroraBackgroundDemo';
+import { NavbarDemo } from './components/NavbarDemo';
+import { TextGenerateEffectDemo } from './components/TextGenerateEffectDemo';
 
 function App() {
 	return (
 		<>
-			<div>
-				<AuroraBackgroundDemo />
-			</div>
-			<div className='flex justify-center'>
-				<ThreeDCardDemo />
-				<ThreeDCardDemo />
-			</div>
-			<div className='bg-gray-500/10 p-5'>
-				<BentoGridDemo />
-			</div>
-			<div>
-				<HeroParallaxDemo />
+			<div className='bg-black'>
+				<div>
+					<NavbarDemo />
+				</div>
+				<div>
+					<AuroraBackgroundDemo />
+				</div>
+				<div className='flex justify-center'>
+					<ThreeDCardDemo />
+					<ThreeDCardDemo />
+				</div>
+				<div className='p-5'>
+					<BentoGridDemo />
+				</div>
+				<div>
+					<HeroParallaxDemo />
+				</div>
+				<div className='h-[500px] w-full p-10 flex justify-center items-center text-center'>
+					<TextGenerateEffectDemo />
+				</div>
 			</div>
 		</>
 	);
