d7bdfb1738c3acb985040b58deafe8cef4cc22b5
SaulQE <saul.esquivel.028@gmail.com>
2024-08-26 23:00:55 -0500

    add GridBackgroundDemo

---
+++ src/components/GridBackgroundDemo.jsx
@@ -0,0 +1,13 @@
+import { TextGenerateEffectDemo } from "./TextGenerateEffectDemo";
+
+export function GridBackgroundDemo() {
+	return (
+		<div className='h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center'>
+			{/* Radial gradient for the container to give a faded look */}
+			<div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
+			<div className='h-[500px] w-full p-10 flex justify-center items-center text-center'>
+				<TextGenerateEffectDemo />
+			</div>
+		</div>
+	);
+}
