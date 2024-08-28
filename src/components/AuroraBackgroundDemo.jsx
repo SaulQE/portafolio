b89569a70015dfc5c2d907df4b662ed4d9863036
SaulQE <saul.esquivel.028@gmail.com>
2024-08-27 23:03:15 -0500

    save

--- src/components/AuroraBackgroundDemo.jsx
+++ src/components/AuroraBackgroundDemo.jsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import { AuroraBackground } from '../ui/aurora-background';
+import { Link } from 'react-router-dom';
 
 export function AuroraBackgroundDemo() {
 	return (
@@ -14,15 +15,21 @@ export function AuroraBackgroundDemo() {
 				}}
 				className='relative flex flex-col gap-4 items-center justify-center px-4'
 			>
-				<div className='text-3xl md:text-7xl font-bold dark:text-white text-center'>
-					Background lights are cool you know.
-				</div>
-				<div className='font-extralight text-base md:text-4xl dark:text-neutral-200 py-4'>
+				<div className='font-extralight text-base md:text-2xl dark:text-neutral-200 pt-2'>
 					Portafolio en desarrollo...
 				</div>
-				<button className='bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2'>
-					Debug now
-				</button>
+				<div className='text-3xl md:text-6xl font-bold dark:text-white text-center'>
+					Saúl Andres Quispesucso Esquivel
+				</div>
+				<p className='text-lg md:text-3xl mb-6 text-gray-400'>
+					DESARROLLADOR DE SOFTWARE
+				</p>
+				<Link
+					to={'/proyectos'}
+					className='bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2'
+				>
+					Mis proyectos
+				</Link>
 			</motion.div>
 		</AuroraBackground>
 	);
