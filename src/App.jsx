1dfd41dd5e207c9418e986d77c34e46a769a6094
SaulQE <saul.esquivel.028@gmail.com>
2024-08-27 21:54:21 -0500

    add component ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { SobreMiPage } from './pages/SobreMiPage';
 import { BlogPage } from './pages/BlogPage';
 import { ContactoPage } from './pages/ContactoPage';
 import { NavbarDemo } from './components/NavbarDemo';
+import { ScrollToTop } from './components/ScrollToTop ';
 
 function App() {
 	return (
 		<>
 			<div className='bg-black'>
 				<Router>
+					<ScrollToTop />
 					<NavbarDemo />
 					<Routes>
 						<Route path='/' element={<HomePage />} />
