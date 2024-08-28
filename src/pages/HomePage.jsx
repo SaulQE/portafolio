3cc689adbc77289648a527ff34fdc52533744016
SaulQE <saul.esquivel.028@gmail.com>
2024-08-27 21:43:54 -0500

    delete navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import { GridBackgroundDemo } from '../components/GridBackgroundDemo';
 import { AuroraBackgroundDemo } from '../components/AuroraBackgroundDemo';
-import { NavbarDemo } from '../components/NavbarDemo';
 
 export const HomePage = () => {
 	return (
 		<>
 			<div>
-				<NavbarDemo />
 				<AuroraBackgroundDemo />
 				<GridBackgroundDemo />
 			</div>
