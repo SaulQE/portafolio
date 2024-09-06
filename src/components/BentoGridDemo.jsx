fa416d36dec8aae216931ae20c84516348fa94db
SaulQE <saul.esquivel.028@gmail.com>
2024-09-05 22:48:46 -0500

    add img

--- src/components/BentoGridDemo.jsx
+++ src/components/BentoGridDemo.jsx
@@ -40,8 +40,7 @@ const items = [
 		title: 'The Dawn of Innovation',
 		description: 'Explore the birth of groundbreaking ideas and inventions.',
 		icon: <IconClipboardCopy className='h-4 w-4 text-neutral-500' />,
-		image:
-			'https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+		image: getImageUrl('ProntoSalud.png'),
 		hoverImage:
 			'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif',
 	},
